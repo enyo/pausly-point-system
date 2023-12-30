8c36f1b173e29a47460476d0b7e17a0aeb27db6a
Matias Meno <m@tias.me>
2023-12-30 16:08:35 +0100

    Store numbers in Hash

--- src/app.d.ts
+++ src/app.d.ts
@@ -5,9 +5,15 @@ declare global {
 		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
-		// interface PageState {}
+		interface PageState {
+			first: number
+			second: number
+			third: number
+			miss: number
+			target: number
+		}
 		// interface Platform {}
 	}
 }
 
-export {};
+export {}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,51 +1,86 @@
 <script lang="ts">
-	import '$lib/style.css';
-	import { intervalToDuration, formatDuration } from 'date-fns';
+	import '$lib/style.css'
+	import { pushState } from '$app/navigation'
+	import { page } from '$app/stores'
+	import { intervalToDuration, formatDuration } from 'date-fns'
+	import { unstate, untrack } from 'svelte'
+	import { browser } from '$app/environment'
+	import { isEqual } from 'lodash-es'
 
-	const numbers = $state({
+	let initialState = {
 		first: 50,
 		second: 30,
 		third: 10,
 		miss: 30,
-		target: 1000
-	});
+		target: 1000,
+	}
+
+	type Numbers = typeof initialState
+
+	if (browser && location.hash) {
+		const params = new URLSearchParams(location.hash.slice(1))
+		if (params.has('first')) {
+			const state = Object.fromEntries(
+				[...params.entries()].map(([key, value]) => [key, +value]),
+			) as Numbers
+			initialState = state
+		}
+	}
+
+	let numbers = $state(initialState)
+
+	$effect(() => {
+		const state = unstate(numbers)
+		const pageState = unstate(untrack(() => $page.state))
+		if (isEqual(state, pageState)) return
+		const params = new URLSearchParams(state)
+		pushState(`#${params}`, unstate(numbers))
+	})
+	$effect(() => {
+		const state = unstate(untrack(() => numbers))
+		const pageState = unstate(untrack(() => $page.state))
+		if ($page.state['first'] && !isEqual(state, pageState)) {
+			numbers = $page.state
+		}
+	})
 
 	const minSessions = () => {
-		let points = 0;
-		let sessions = 0;
+		let points = 0
+		let sessions = 0
 
 		do {
-			sessions++;
-			points = numbers.first * sessions - (7 - sessions) * numbers.miss;
-		} while (points <= 0 && sessions < 7);
+			sessions++
+			points = numbers.first * sessions - (7 - sessions) * numbers.miss
+		} while (points <= 0 && sessions < 7)
 
-		return sessions;
-	};
+		return sessions
+	}
 
-	let fastest = $derived(numbers.target / (numbers.first + numbers.second + numbers.third));
-	let minNumberOfSessionsPerWeek = $derived(minSessions());
+	let fastest = $derived(numbers.target / (numbers.first + numbers.second + numbers.third))
+	let minNumberOfSessionsPerWeek = $derived(minSessions())
 	let fastestWithWeekends = $derived(
-		numbers.target / (((numbers.first + numbers.second + numbers.third) * 5 - numbers.miss * 2) / 7)
-	);
+		numbers.target /
+			(((numbers.first + numbers.second + numbers.third) * 5 - numbers.miss * 2) / 7),
+	)
 	let fastestWithWeekendsOnly1 = $derived(
-		numbers.target / ((numbers.first * 5 - numbers.miss * 2) / 7)
-	);
+		numbers.target / ((numbers.first * 5 - numbers.miss * 2) / 7),
+	)
 	let mostCommon = $derived(
-		numbers.target / ((numbers.first * 4 + numbers.second * 2 - numbers.miss * 4) / 7)
-	);
+		numbers.target / ((numbers.first * 4 + numbers.second * 2 - numbers.miss * 4) / 7),
+	)
 	let powerUser = $derived(
 		numbers.target /
-			((numbers.first * 6 + numbers.second * 4 + numbers.third - numbers.miss * 1) / 7)
-	);
+			((numbers.first * 6 + numbers.second * 4 + numbers.third - numbers.miss * 1) / 7),
+	)
 	let timeWithMinimum = $derived(
-		numbers.target / ((numbers.first * minNumberOfSessionsPerWeek) / 7)
-	);
+		numbers.target / ((numbers.first * minNumberOfSessionsPerWeek) / 7),
+	)
 
 	const formatDays = (days: number) => {
 		return formatDuration(intervalToDuration({ start: 0, end: days * 24 * 60 * 60 * 1000 }), {
-			format: ['years', 'months', 'weeks', 'days']
-		});
-	};
+			format: ['years', 'months', 'weeks', 'days'],
+		})
+	}
 </script>
 
 <div class="values">
@@ -57,33 +92,39 @@
 </div>
 
 <button
-	onclick={() => (
-		(numbers.first = 50),
-		(numbers.second = 30),
-		(numbers.third = 10),
-		(numbers.miss = 30),
-		(numbers.target = 1000)
-	)}>Option 1</button
+	onclick={() => {
+		numbers = {
+			first: 50,
+			second: 30,
+			third: 10,
+			miss: 30,
+			target: 1000,
+		}
+	}}>Option 1</button
 >
 
 <button
-	onclick={() => (
-		(numbers.first = 40),
-		(numbers.second = 20),
-		(numbers.third = 10),
-		(numbers.miss = 20),
-		(numbers.target = 1000)
-	)}>Option 2</button
+	onclick={() => {
+		numbers = {
+			first: 40,
+			second: 20,
+			third: 10,
+			miss: 20,
+			target: 1000,
+		}
+	}}>Option 2</button
 >
 
 <button
-	onclick={() => (
-		(numbers.first = 40),
-		(numbers.second = 20),
-		(numbers.third = 10),
-		(numbers.miss = 15),
-		(numbers.target = 1000)
-	)}>Option 3</button
+	onclick={() => {
+		numbers = {
+			first: 40,
+			second: 20,
+			third: 10,
+			miss: 15,
+			target: 1000,
+		}
+	}}>Option 3</button
 >
 
 <hr />
